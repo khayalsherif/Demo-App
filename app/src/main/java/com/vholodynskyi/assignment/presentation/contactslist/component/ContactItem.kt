8b926e4566fe9f3997292d05bf825eed44851965
khayalsharifli <xeyalsherif@gmail.com>
2023-01-31 20:46:48 +0400

    add pull refresh with compose

--- app/src/main/java/com/vholodynskyi/assignment/presentation/contactslist/component/ContactItem.kt
+++ app/src/main/java/com/vholodynskyi/assignment/presentation/contactslist/component/ContactItem.kt
@@ -2,16 +2,12 @@ package com.vholodynskyi.assignment.presentation.contactslist.component
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vholodynskyi.assignment.domain.model.Contact
 
--- app/src/main/java/com/vholodynskyi/assignment/presentation/contactslist/component/ContactListScreen.kt
+++ app/src/main/java/com/vholodynskyi/assignment/presentation/contactslist/component/ContactListScreen.kt
@@ -6,9 +6,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
+import com.github.fengdai.compose.pulltorefresh.PullToRefresh
+import com.github.fengdai.compose.pulltorefresh.PullToRefreshState
 import com.vholodynskyi.assignment.presentation.Screen
 import com.vholodynskyi.assignment.presentation.contactslist.ContactListViewModel
 import org.koin.androidx.compose.koinViewModel
@@ -18,24 +22,34 @@ fun ContactListScreen(
     navController: NavController,
     viewModel: ContactListViewModel = koinViewModel()
 ) {
+    val isRefreshing = rememberSaveable { mutableStateOf(false) }
     val context = LocalContext.current
     val state = viewModel.contactResponse.value
 
-    if (state.contact.isNotEmpty()) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-                items(state.contact) { contact ->
-                    ContactItem(
-                        contact = contact,
-                        onItemClick = {
-                            navController.navigate(Screen.ContactDetail.route + "/${contact.id}")
-                        }
-                    )
+    PullToRefresh(state = PullToRefreshState(isRefreshing.value),
+        onRefresh = {
+            isRefreshing.value = true
+            viewModel.syncData()
+        }
+    ) {
+        if (state.contact.isNotEmpty()) {
+            if (isRefreshing.value) isRefreshing.value = false
+            Box(modifier = Modifier.fillMaxSize()) {
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    items(state.contact) { contact ->
+                        ContactItem(
+                            contact = contact,
+                            onItemClick = {
+                                navController.navigate(Screen.ContactDetail.route + "/${contact.id}")
+                            }
+                        )
+                    }
                 }
             }
-        }
-        if (state.error.isNotEmpty()) {
-            Toast.makeText(context, state.error, Toast.LENGTH_LONG).show()
+            if (state.error.isNotEmpty()) {
+                Toast.makeText(context, state.error, Toast.LENGTH_LONG).show()
+            }
         }
     }
+
 }
