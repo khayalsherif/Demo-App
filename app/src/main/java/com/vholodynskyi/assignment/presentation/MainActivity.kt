e83067a6116f464acc2fdc845881b9e9d479f12c
khayalsharifli <xeyalsherif@gmail.com>
2023-02-06 17:27:10 +0400

    add feature: navigation animation

--- app/src/main/java/com/vholodynskyi/assignment/presentation/MainActivity.kt
+++ app/src/main/java/com/vholodynskyi/assignment/presentation/MainActivity.kt
@@ -3,10 +3,16 @@ package com.vholodynskyi.assignment.presentation
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.*
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.material.Surface
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.IntOffset
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.vholodynskyi.assignment.presentation.contactslist.component.ContactListScreen
 import com.vholodynskyi.assignment.presentation.details.component.DetailsScreen
 import com.vholodynskyi.assignment.presentation.edit.compenent.EditScreen
@@ -17,28 +23,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ContactAppTheme {
-                Surface {
-                    val navController = rememberNavController()
-                    NavHost(
-                        navController = navController,
-                        startDestination = Screen.ContactList.route
-                    ) {
-                        composable(
-                            route = Screen.ContactList.route
-                        ) {
-                            ContactListScreen(navController)
-                        }
-                        composable(
-                            route = Screen.ContactDetail.route + "/{contactId}"
-                        ) {
-                            DetailsScreen(navController = navController)
-                        }
-                        composable(
-                            route = Screen.ContactEdit.route + "/{contactId}"
-                        ) {
-                            EditScreen(navController = navController)
-                        }
+                NavigationComponent()
+            }
+        }
+    }
+
+    @OptIn(ExperimentalAnimationApi::class)
+    @Composable
+    fun NavigationComponent() {
+        Surface {
+            val navController = rememberAnimatedNavController()
+            val springSpec = spring<IntOffset>(stiffness = Spring.StiffnessMediumLow)
+            AnimatedNavHost(
+                navController = navController,
+                startDestination = Screen.ContactList.route
+            ) {
+                composable(
+                    route = Screen.ContactList.route,
+                    enterTransition = {
+                        slideInHorizontally(initialOffsetX = { 1000 }, animationSpec = springSpec)
+                    },
+                    exitTransition = {
+                        slideOutHorizontally(targetOffsetX = { -1000 }, animationSpec = springSpec)
+                    },
+                    popEnterTransition = {
+                        slideInHorizontally(initialOffsetX = { -1000 }, animationSpec = springSpec)
+                    },
+                    popExitTransition = {
+                        slideOutHorizontally(targetOffsetX = { 1000 }, animationSpec = springSpec)
                     }
+                ) {
+                    ContactListScreen(navController)
+                }
+                composable(
+                    route = Screen.ContactDetail.route + "/{contactId}"
+                ) {
+                    DetailsScreen(navController = navController)
+                }
+                composable(
+                    route = Screen.ContactEdit.route + "/{contactId}"
+                ) {
+                    EditScreen(navController = navController)
                 }
             }
         }
